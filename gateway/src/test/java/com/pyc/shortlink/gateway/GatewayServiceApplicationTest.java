48ed72b5ed5ca1c07b1154969d5fb4be83c8b987
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2025-05-14 13:34:59 +0800

    后端网关

---
+++ gateway/src/test/java/com/pyc/shortlink/gateway/GatewayServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.pyc.shortlink.gateway;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+class GatewayServiceApplicationTest {
+
+}
